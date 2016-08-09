0d2e7dededc5914f5ad785b1172c2f9bb38859da
Michael <mike.hunsinger@gmail.com>
2016-08-09 15:46:52 -0600

    Added marginalia and midje dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject majrix "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Matrix chat client written in Clojure"
+  :url "https://www.github.com/jeff-flower/Majrix"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [midje "1.8.3"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-marginalia "0.8.0"]]
   :ring {:handler majrix.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
