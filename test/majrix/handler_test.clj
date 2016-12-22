4a945f54cf6de458c7f2d1ae7a14ed0cc09299da
Jeff <jeff.flower14@gmail.com>
2016-12-21 21:29:21 -0700

    Finish first swag at route tests
    See notes for gotchas using ring.mock.request

--- test/majrix/handler_test.clj
+++ test/majrix/handler_test.clj
@@ -1,7 +1,8 @@
 (ns majrix.handler-test
   (:require [midje.sweet :refer :all] 
             [ring.mock.request :as mock]
-            [majrix.handler :refer :all]))
+            [majrix.handler :refer :all]
+            [cheshire.core :as cheshire]))
 
 (facts "compose-response"
   (let [userid "user1"]
@@ -14,11 +15,30 @@
       (contains? (:body (compose-response userid {:error :NOT_AN_ERROR})) :error) => true)))
 
 (facts "register route"
-  (facts "successful response"
-    (fact "returns status of 200"
-      ;; TODO: add correct parameters to mock request and handle appropriately in prerequisite
-      (prerequisite (#'majrix.db/create-user! ..userid.. ..homeserver..) => {})
-      (:status (app (mock/request :post "/_matrix/client/r0/register"))) => 200)
-    (facts "error response"
-      (fact "returns status of 400")
-      (fact "returns error message"))))
+  (let [route "/_matrix/client/r0/register"
+        username "user1"
+        body (cheshire/generate-string {:username username})]
+    (facts "successful response"
+      (fact "returns status of 200"
+        ;;
+        (:status (app (-> (mock/request :post route)
+                          ;; must set request content type or body content won't be added correctly
+                          (mock/content-type "application/json")
+                          (mock/body body)))) => 200
+        ;; provided is midje mocking function that lays out conditions that must exist for a test to be successful
+        ;; note that you must use the full namespace qualified name of var you wish to redefine if the var belongs
+        ;; to a namespace different from the namespace you are testing
+        (provided (#'majrix.db/create-user! username "majrix") => {}))
+      (facts "error response"
+        (prerequisite (#'majrix.db/create-user! username "majrix") => {:error :M_USER_IN_USE})
+        (fact "returns status of 400"
+          (:status (app (-> (mock/request :post route)
+                            ;; must set request content type or body content won't be added correctly
+                            (mock/content-type "application/json")
+                            (mock/body body)))) => 400)
+        (fact "returns error message"
+          (contains? (app (-> (mock/request :post route)
+                              ;; must set request content type or body content won't be added correctly
+                              (mock/content-type "application/json")
+                              (mock/body body))) :body) => true)))))
+
