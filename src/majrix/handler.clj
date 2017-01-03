f2fd3f0c956484790ac01ace3c6412d85d6a34fb
Michael Hunsinger <mike.hunsinger@gmail.com>
2017-01-02 19:16:06 -0700

    Readded compose-response after accidentially killing it.

--- src/majrix/handler.clj
+++ src/majrix/handler.clj
@@ -15,8 +15,8 @@
                 slurp
                 edn/read-string))
 
-;; A map defining all of the API responses based on error codes returned from
-;; the database layer.
+; map a matrix error code returned by the db side to a set of values suitable to
+; return from the api to the user
 (def db-error-map {:M_USER_IN_USE {:status 400
                                    :message "Username in use."}})
 
@@ -35,14 +35,27 @@
   [username]
   (cheshire/generate-string {:username username}))
 
+(defn compose-response
+  [{error-code :error} api-res]
+  (if (nil? error-code)
+    (ring-response/response api-res)
+    (let [error-map (error-code db-error-map)]
+      (-> {:error (:message error-map)}
+          (ring-response/response)
+          (ring-response/status (:status error-map))))))
+
 (defroutes app-routes
+  ;; TODO create a route to login, create a room, join room, get
+  ;; message, and send message. Recommended to work on the handle
+  ;; portion first before moving onto the database side. Checkout the
+  ;; Matrix spec and Client-Server guide for more information/help.
   (context "/_matrix/client/r0" []
            (POST "/register" req (fn [req]
                                    (let [userid (get-in req [:body "username"])]
                                      (-> (db/create-user! userid (:home-server config))
                                          (compose-response {:user_id userid
-                                                            :access_token (generate-access-token userid)
-                                                            :home_server (:home-server config)})))))
+                                                             :access_token (generate-access-token userid)
+                                                             :home_server (:home-server config)})))))
            (POST "/login" req (fn [req] (ring-response/response req)))
            (POST "/createRoom" req (fn [req] (ring-response/response req)))
            (POST "/join/:roomId" [roomId] (str roomId)) 
@@ -51,7 +64,10 @@
 
 (def app
   (-> app-routes
-      ring-json/wrap-json-body          ; parse the incoming request to map
-      ring-json/wrap-json-response      ; parse the outgoing response to json
+      ; wrap-json-body: parse the body of a request with JSON content-type into a map and assign it to the :body key
+      ring-json/wrap-json-body
+      ; wrap-json-response: convert a response with a clojure collection as a body into JSON
+      ring-json/wrap-json-response
+      ; (wrap-defaults handler site-defaults)
       ; wrap-defaults sets up standard ring middleware
       (ring-defaults/wrap-defaults (assoc-in ring-defaults/site-defaults [:security :anti-forgery] false))))
